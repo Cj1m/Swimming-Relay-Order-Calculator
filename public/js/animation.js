86a5ea800f51e879d527a3cd95eff473fe39345c
C4R15 <chrispercevalmaxwell@gmail.com>
2017-10-25 14:46:07 +0100

    header animations and bg

---
+++ public/js/animation.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $(function() {
+    switch (window.location.pathname) {
+      case '/':
+        $('li#nav-home').addClass('nav-active');
+        $('li#nav-home a').removeClass('a-nav-hover');
+        break;
+      case '/addrecords':
+        $('li#nav-a-records').addClass('nav-active');
+        $('li#nav-a-records a').removeClass('a-nav-hover');
+        break;
+      case '/viewrecords':
+        $('li#nav-v-records').addClass('nav-active');
+        $('li#nav-v-records a').removeClass('a-nav-hover');
+        break;
+      case '/calculator':
+        $('li#nav-calc').addClass('nav-active');
+        $('li#nav-calc a').removeClass('a-nav-hover');
+        break;
+      case '/results':
+        $('li#nav-results').addClass('nav-active');
+        $('li#nav-results a').removeClass('a-nav-hover');
+        break;
+    }
+  });
+})
